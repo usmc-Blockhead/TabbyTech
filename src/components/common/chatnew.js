8be568eadae0eb8ca430b3972ee47b65d4cb6520
Christa <jaysplaypen@gmail.com>
2024-09-06 10:06:04 -0700

    update

---
+++ src/components/common/chatnew.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ChatBot from "react-chatbotify";
+
+const MyChatBot = () => {
+	let apiKey = null;
+	let modelType = "gpt-3.5-turbo";
+	let hasError = false;
+
+function chatnew = async (params) => {
+    return <div>chatnew</div>;
+}
+
+export default chatnew;
